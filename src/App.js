1680397ee4767125f56a24817c6089c4f7878711
Andrii Rudkovskyi <arudkovskyy@Andriys-MacBook-Pro.local>
2022-02-19 20:03:52 +0200

    add layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Dopomoga } from "./components/Dopomoga/Dopomoga";
+import { PersonalData } from "./components/PersonalData/PersonalData";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonalData />
+      <Dopomoga />
     </div>
   );
 }
